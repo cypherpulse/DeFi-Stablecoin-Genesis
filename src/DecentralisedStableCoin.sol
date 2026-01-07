66623d13685b166e10e1d29d05ede09e55e6f21b
cypherpulse <johnbradill67@gmail.com>
2026-01-07 05:54:22 +0300

    Initialized DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - src/DecentralisedStableCoin.sol

---
+++ src/DecentralisedStableCoin.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volatility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/*
+*@title: Decentralised Stable Coin
+*@author: cypherpulse.base.eth
+*collateral : Exogenous(ETH & BTC)
+*Minting: Algorithmic
+*Relative Stability: Coin pegged to USD
+*
+* This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+*/
+
+contract DecentralizedStableCoin{}
