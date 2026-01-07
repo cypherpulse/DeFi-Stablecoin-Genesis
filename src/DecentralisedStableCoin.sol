c7f2439c239a8dbabfe3ad76790c9a31da5dde71
cypherpulse <johnbradill67@gmail.com>
2026-01-07 06:03:39 +0300

    Initialized DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - src/DecentralisedStableCoin.sol

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -35,4 +35,9 @@ import {ERC20Burnable,ERC20} from "@openzeppelin/contracts/token/ERC20/extension
 * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
 */
 
-contract DecentralizedStableCoin{}
+contract DecentralizedStableCoin is ERC20Burnable{
+    /////////////////
+    // Constructor //
+    /////////////////
+    constructor() ERC20("DecentralizedStableCoin", "DSC"){}
+}
