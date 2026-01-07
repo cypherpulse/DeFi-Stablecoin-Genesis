d029d99e8e72e6fbd295b6a94aff60d6dc18bf96
cypherpulse <johnbradill67@gmail.com>
2026-01-07 06:17:19 +0300

    Initialized DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - src/DecentralisedStableCoin.sol

--- src/DecentralisedStableCoin.sol
+++ src/DecentralisedStableCoin.sol
@@ -40,12 +40,26 @@ contract DecentralizedStableCoin is ERC20Burnable{
     //Errors
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
     /////////////////
     // Constructor //
     /////////////////
     constructor() ERC20("DecentralizedStableCoin", "DSC"){}
 
-    function burn (uint256 _amount) public override Only{
+    function burn (uint256 _amount) public override onlyOwner{
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <=0){
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if(balance < _amount){
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
 
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
+        if(_to == address(0)){
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
     }
 }
