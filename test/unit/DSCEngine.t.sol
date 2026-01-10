ead6e12dfe215ccfd9e0f94dced79472cf2e3479
cypherpulse <johnbradill67@gmail.com>
2026-01-10 12:56:22 +0300

    updated DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/unit/DSCEngine.t.sol

---
+++ test/unit/DSCEngine.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract DSCEngineTest is Test {}
