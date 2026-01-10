f43c3e8d96b24f2828c9a2f4a8bded2f4344eed2
cypherpulse <johnbradill67@gmail.com>
2026-01-10 13:09:25 +0300

    updated DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/unit/DSCEngine.t.sol

--- test/unit/DSCEngine.t.sol
+++ test/unit/DSCEngine.t.sol
@@ -4,14 +4,30 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {Test, console} from "forge-std/Test.sol";
+import { HelperConfig } from "../../script/HelperConfig.s.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
     DecentralizedStableCoin dsc;
     DSCEngine dsce;
+    HelperConfig config;
+    address weth;
+    address ethUsdPriceFeed;
 
-    function setUp() public{
+    function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce,) = deployer.run();
+        (ethUsdPriceFeed,,weth,,)=config.activeNetworkConfig();
+    }
+
+    /////////////////
+    // Price Tests //
+    /////////////////
+    function testGetUsdValue() public {
+        //15e18*2000/ETH = 30,000e18
+        uint256 ethAmount = 15e18;
+        uint256 expectedUsd = 30000e18;
+        uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
+        assertEq(expectedUsd, actualUsd);
     }
 }
