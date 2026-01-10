007ba4eaa785ab4f62f692bc707919b49bfa052e
cypherpulse <johnbradill67@gmail.com>
2026-01-10 13:18:07 +0300

    updated DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/unit/DSCEngine.t.sol

--- test/unit/DSCEngine.t.sol
+++ test/unit/DSCEngine.t.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {Test, console} from "forge-std/Test.sol";
-import { HelperConfig } from "../../script/HelperConfig.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../../test/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -14,10 +15,16 @@ contract DSCEngineTest is Test {
     address weth;
     address ethUsdPriceFeed;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce,) = deployer.run();
-        (ethUsdPriceFeed,,weth,,)=config.activeNetworkConfig();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     /////////////////
@@ -29,5 +36,16 @@ contract DSCEngineTest is Test {
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
         assertEq(expectedUsd, actualUsd);
+    }
+
+    /////////////////////////////
+    // depositCollateral Tests //
+    /////////////////////////////
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
