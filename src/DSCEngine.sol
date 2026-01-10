4dfa8a8aefa3ff2f27338b6debe15a6774a53500
cypherpulse <johnbradill67@gmail.com>
2026-01-10 12:56:23 +0300

    updated DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - src/DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -32,7 +32,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 /**
  * @title DSCEngine
- * @author Patrick Collins
+ * @author cypherpulse.base.eth
  *
  * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg at all times.
  * This is a stablecoin with the properties:
