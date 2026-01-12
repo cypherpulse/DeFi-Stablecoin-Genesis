6f43a535786bd31abe04daee3f4012d35e0cffce
cypherpulse <johnbradill67@gmail.com>
2026-01-12 21:06:45 +0300

    updated DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - src/DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -212,21 +212,21 @@ contract DSCEngine is ReentrancyGuard {
         _revertIfHealthFactorIsBroken(msg.sender);
     }
 
-    /** 
-    * @param collateral: The ERC20 token address of the collateral you're using to make the protocol solvent again.
-    * This is collateral that you're going to take from the user who is insolvent.
-    * In return, you have to burn your DSC to pay off their debt, but you don't pay off your own.
-    * @param user: The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR
-    * @param debtToCover: The amount of DSC you want to burn to cover the user's debt.
-    *
-    * @notice: You can partially liquidate a user.
-    * @notice: You will get a 10% LIQUIDATION_BONUS for taking the users funds.
-    * @notice: This function working assumes that the protocol will be roughly 150% overcollateralized in order for this
-    to work.
-    * @notice: A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate
-    anyone.
-    * For example, if the price of the collateral plummeted before anyone could be liquidated.
-    */
+    /**
+     * @param collateral: The ERC20 token address of the collateral you're using to make the protocol solvent again.
+     * This is collateral that you're going to take from the user who is insolvent.
+     * In return, you have to burn your DSC to pay off their debt, but you don't pay off your own.
+     * @param user: The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR
+     * @param debtToCover: The amount of DSC you want to burn to cover the user's debt.
+     *
+     * @notice: You can partially liquidate a user.
+     * @notice: You will get a 10% LIQUIDATION_BONUS for taking the users funds.
+     * @notice: This function working assumes that the protocol will be roughly 150% overcollateralized in order for this
+     * to work.
+     * @notice: A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate
+     * anyone.
+     * For example, if the price of the collateral plummeted before anyone could be liquidated.
+     */
 
     function liquidate(address collateral, address user, uint256 debtToCover)
         external
@@ -273,6 +273,17 @@ contract DSCEngine is ReentrancyGuard {
     ///////////////////////////////////////////
     //    Private & Internal View Functions  //
     ///////////////////////////////////////////
+
+    function _redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral, address from, address to)
+        private
+    {
+        s_collateralDeposited[from][tokenCollateralAddress] -= amountCollateral;
+        emit CollateralRedeemed(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transfer(to, amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
 
     function _revertIfHealthFactorIsBroken(address user) internal view {
         uint256 userHealthFactor = _healthFactor(user);
