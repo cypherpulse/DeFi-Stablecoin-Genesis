6980e4a9601915445dabaa4b3ed2fe6b342ba9b4
cypherpulse <johnbradill67@gmail.com>
2026-01-13 14:18:52 +0300

    Handler-based Invariants Fuzz tests for  DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/fuzz/Handler.t.sol

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -4,4 +4,6 @@
 *Handler is going to narrow down the way our fuzzer tests work by specifying certain functions to be called and certain
 * preconditions to be met before those functions are called.
 *This allows us to have more control over the fuzz testing process and focus on specific areas of our contract that we want to test more thoroughly.
- */
+*/
+
+ pragma solidity ^0.8.20;
