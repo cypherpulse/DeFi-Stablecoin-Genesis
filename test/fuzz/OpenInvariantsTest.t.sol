2edeb2a7945ae3af4491e56dd65d8af9f0b939b6
cypherpulse <johnbradill67@gmail.com>
2026-01-13 14:12:33 +0300

    Modified OpenInvariants Fuzz tests for  DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/fuzz/OpenInvariantsTest.t.sol

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,55 +1,55 @@
-//SPDX-License-Identifier: MIT
+// //SPDX-License-Identifier: MIT
 
-/***
-*Before writing a single line of our invariant tests we need to ask the question
+// /***
+// *Before writing a single line of our invariant tests we need to ask the question
 
-*What are the invariants of my protocol?
+// *What are the invariants of my protocol?
 
-*We need to ascertain which properties of our system must always hold. What are some for DecentralizedStableCoin?
+// *We need to ascertain which properties of our system must always hold. What are some for DecentralizedStableCoin?
 
-*    1.The total supply of DSC should be less than the total value of collateral
+// *    1.The total supply of DSC should be less than the total value of collateral
 
-*    2. Getter view functions should never revert
- */
+// *    2. Getter view functions should never revert
+//  */
 
-pragma solidity ^0.8.20;
+// pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
-import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {DeployDSC} from "../../script/DeployDSC.s.sol";
-import {DSCEngine} from "../../src/DSCEngine.sol";
-import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
-import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {Test, console} from "forge-std/Test.sol";
+// import {StdInvariant} from "forge-std/StdInvariant.sol";
+// import {DeployDSC} from "../../script/DeployDSC.s.sol";
+// import {DSCEngine} from "../../src/DSCEngine.sol";
+// import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+// import {HelperConfig} from "../../script/HelperConfig.s.sol";
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract OpenInvariantsTest is StdInvariant, Test {
-    DeployDSC deployer;
-    DSCEngine dsce;
-    DecentralizedStableCoin dsc;
-    HelperConfig config;
+// contract OpenInvariantsTest is StdInvariant, Test {
+//     DeployDSC deployer;
+//     DSCEngine dsce;
+//     DecentralizedStableCoin dsc;
+//     HelperConfig config;
 
-    address weth;
-    address wbtc;
+//     address weth;
+//     address wbtc;
 
-    function setUp() external {
-        deployer = new DeployDSC();
-        (dsc, dsce, config) = deployer.run();
-        (,, weth, wbtc,) = config.activeNetworkConfig();
-        targetContract(address(dsce));
-    }
+//     function setUp() external {
+//         deployer = new DeployDSC();
+//         (dsc, dsce, config) = deployer.run();
+//         (,, weth, wbtc,) = config.activeNetworkConfig();
+//         targetContract(address(dsce));
+//     }
 
-    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
-        uint256 totalSupply = dsc.totalSupply();
-        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
-        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+//     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+//         uint256 totalSupply = dsc.totalSupply();
+//         uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+//         uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
 
-        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
-        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+//         uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+//         uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
 
-        console.log("Weth Value: ", wethValue);
-        console.log("Wbtc Value: ", wbtcValue);
-        console.log("Total Supply: ", totalSupply);
+//         console.log("Weth Value: ", wethValue);
+//         console.log("Wbtc Value: ", wbtcValue);
+//         console.log("Total Supply: ", totalSupply);
 
-        assert(wethValue + wbtcValue >= totalSupply);
-    }
-}
+//         assert(wethValue + wbtcValue >= totalSupply);
+//     }
+// }
