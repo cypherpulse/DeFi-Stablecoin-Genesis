7fab2a97e04b027f7478538ae41edfcc9f3e541e
cypherpulse <johnbradill67@gmail.com>
2026-01-13 14:37:24 +0300

    Handler-based Invariants Fuzz tests for  DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/fuzz/Handler.t.sol

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -8,5 +8,7 @@
 
 pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
 contract Handler is Test {}
