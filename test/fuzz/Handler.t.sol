a9f3e935526efe2bd6199249076c175f28284f32
cypherpulse <johnbradill67@gmail.com>
2026-01-13 14:50:29 +0300

    Handler-based Invariants Fuzz tests for  DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/fuzz/Handler.t.sol

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -10,6 +10,7 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract Handler is Test {
     DSCEngine dsce;
@@ -17,6 +18,8 @@ contract Handler is Test {
 
     ERC20Mock weth;
     ERC20Mock wbtc;
+
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
 
     constructor(DSCEngine _engine, DecentralizedStableCoin _dsc) {
         dsce = _engine;
@@ -27,7 +30,24 @@ contract Handler is Test {
         wbtc = ERC20Mock(collateralTokens[1]);
     }
 
+    // Helper Functions
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        }
+        return wbtc;
+    }
+
     function depositCollateral(uint256 collateral, uint256 amountCollateral) public {
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
         dsce.depositCollateral(collateral, amountCollateral);
+
+        // mint and approve!
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(engine), amountCollateral);
+        engine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
     }
 }
