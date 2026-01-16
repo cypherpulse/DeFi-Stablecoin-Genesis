ca1e40d7add716dabd854d953a2099b6dad8e125
cypherpulse <johnbradill67@gmail.com>
2026-01-16 12:37:46 +0300

    Using the Oracle Library created in DSCEngine.sol  on  DecentralisedStableCoin contract on Base blockchain using Solidity ^0.8.24, OpenZeppelin, and Foundry for decentralized stablecoin implementation - test/fuzz/OpenInvariantsTest.t.sol

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,18 +1,18 @@
-// //SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// /***
-// *Before writing a single line of our invariant tests we need to ask the question
+/***
+*Before writing a single line of our invariant tests we need to ask the question
 
-// *What are the invariants of my protocol?
+*What are the invariants of my protocol?
 
-// *We need to ascertain which properties of our system must always hold. What are some for DecentralizedStableCoin?
+*We need to ascertain which properties of our system must always hold. What are some for DecentralizedStableCoin?
 
-// *    1.The total supply of DSC should be less than the total value of collateral
+*    1.The total supply of DSC should be less than the total value of collateral
 
-// *    2. Getter view functions should never revert
-//  */
+*    2. Getter view functions should never revert
+ */
 
-// pragma solidity ^0.8.20;
+pragma solidity ^0.8.20;
 
 // import {Test, console} from "forge-std/Test.sol";
 // import {StdInvariant} from "forge-std/StdInvariant.sol";
